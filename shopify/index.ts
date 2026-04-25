450be587b1e6213a898595c398cfe98df8de0938
johnonyango <jpattrick538@gmail.com>
2026-04-25 12:40:11 +0300

    update storefront api version

--- shopify/index.ts
+++ shopify/index.ts
@@ -2,6 +2,6 @@ import { createStorefrontApiClient } from '@shopify/storefront-api-client'
 
 export const storefront = createStorefrontApiClient({
   storeDomain: process.env.SHOPIFY_STORE_DOMAIN!,
-  apiVersion: '2024-01',
+  apiVersion: '2025-07',
   publicAccessToken: process.env.STOREFRONT_API_TOKEN!,
 })
